6a3ead335e9cde2df00c2422324e5c9204c40d7c
Alan-Augusto <augustoalan56@gmail.com>
2022-06-21 20:01:34 -0300

    tentativa 01

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,46 @@
 #include <iostream>
-#include <string>
+#include <sstream>
+#include <string.h>
 
 using namespace std;
 
+int main() {
 
-int main(){
-    cout << "HELLO WORLD!" << endl;
-     
+    int numtextos;
+    string texto;
+    string palavra;
+    
+    
+    cin >> numtextos;
+
+    string lixo;
+    getline(cin, lixo);
+
+    for(int i = 0; i< numtextos; i++){
+        getline(cin, texto);
+        getline(cin, palavra);
+        stringstream textoaux(texto);
+        string palavraux;
+        int countwords = 0;
+
+        int tamtexto = texto.length();
+        for(int j=0; j<tamtexto; j++){
+            if(texto[j-1] == ' ' || j==0){
+                textoaux >> palavraux;
+                if(palavra == palavraux){
+                    if(countwords >0){
+                        cout << " ";
+                    }
+                    cout << j;
+                    countwords++;
+
+                }
+            }
+            if((j+1==tamtexto)&& countwords == 0){
+                cout << -1;
+            }
+        }
+        cout << "" << endl;
+    }
+
 }
