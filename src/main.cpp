0ddddedd0aa989da81074c2a939db7901da4cf3f
Alan-Augusto <augustoalan56@gmail.com>
2022-06-21 20:10:37 -0300

    Alterações finais de organização

--- src/main.cpp
+++ src/main.cpp
@@ -4,43 +4,48 @@
 
 using namespace std;
 
-int main() {
+int main()
+{
 
     int numtextos;
     string texto;
     string palavra;
-    
-    
+
     cin >> numtextos;
 
     string lixo;
     getline(cin, lixo);
 
-    for(int i = 0; i< numtextos; i++){
+    for (int i = 0; i < numtextos; i++)
+    {
+        bool temPalavra = false;
         getline(cin, texto);
         getline(cin, palavra);
         stringstream textoaux(texto);
         string palavraux;
-        int countwords = 0;
 
         int tamtexto = texto.length();
-        for(int j=0; j<tamtexto; j++){
-            if(texto[j-1] == ' ' || j==0){
+        for (int j = 0; j < tamtexto; j++)
+        {
+            if (texto[j - 1] == ' ' || j == 0)
+            {
                 textoaux >> palavraux;
-                if(palavra == palavraux){
-                    if(countwords >0){
+                if (palavra == palavraux)
+                {
+                    if (temPalavra)
+                    {
                         cout << " ";
                     }
                     cout << j;
-                    countwords++;
-
+                    temPalavra = true;
                 }
             }
-            if((j+1==tamtexto)&& countwords == 0){
-                cout << -1;
-            }
         }
-        cout << "" << endl;
-    }
 
+        if (!temPalavra)
+        {
+            cout << -1;
+        }
+        cout << endl;
+    }
 }
